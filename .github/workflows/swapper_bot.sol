e77fb65da7501269c36d2653ac653047acdd525c
kayamavy <150877815+maryoa61@users.noreply.github.com>
2026-06-24 22:44:05 +0330

    Update token addresses in swapper_bot.sol

--- .github/workflows/swapper_bot.sol
+++ .github/workflows/swapper_bot.sol
@@ -21,8 +21,8 @@ jobs:
           PK_1: ${{ secrets.PK_1 }}
           PK_2: ${{ secrets.PK_2 }}
           PK_3: ${{ secrets.PK_3 }}
-          TOKEN_A: "0x........................................" # <-- آدرس توکن اول
-          TOKEN_B: "0x........................................" # <-- آدرس توکن دوم
+          TOKEN_A: "0x047d2881b485519c793e0decd4dbd08c2e142b7e" # <-- آدرس توکن اول
+          TOKEN_B: "0x21b26889a98b44f60bc0819fbadc4e89b48d5ceb" # <-- آدرس توکن دوم
           RPC_URL: "https://rpc.testnet.chain.robinhood.com/"
         run: |
           forge script script/AutoSwapper.s.sol:AutoSwapperScript \
